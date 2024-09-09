176bc608f90504e9cd99606a9e0c706064bf5ed7
Josué Clément <josue.d.clement@gmail.com>
2024-09-09 15:44:12 +0200

    Finalize tester

--- Tester/App.xaml.cs
+++ Tester/App.xaml.cs
@@ -9,4 +9,12 @@ namespace Tester;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var window = new MainWindow();
+        var vm = new MainWindowViewModel();
+        window.DataContext = vm;
+        window.Show();
+    }
 }
---
+++ Tester/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MdiWpf;
+
+namespace Tester;
+
+public class MainWindowViewModel : ObservableObject
+{
+    public MainWindowViewModel()
+    {
+        Brush = Brushes.Blue;
+        Geometry = IconsManager.GetIconGeometry("source-repository");
+        ChangeIconCommand = new RelayCommand(ChangeIcon);
+    }
+    
+    public Brush? Brush
+    {
+        get => _brush;
+        set => SetProperty(ref _brush, value);
+    }
+    private Brush? _brush;
+
+    public Geometry? Geometry
+    {
+        get => _geometry;
+        set => SetProperty(ref _geometry, value);
+    }
+    private Geometry? _geometry;
+    
+    public RelayCommand ChangeIconCommand { get; }
+
+    private void ChangeIcon()
+    {
+        Geometry = IconsManager.GetIconGeometry("sync-circle");
+        Brush = Brushes.Red;
+    }
+}
